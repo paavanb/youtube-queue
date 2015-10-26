fa5e7f50ad1a3a883cedca824563c564c2f1a014
Paavan Bhavsar <pbhavsar@kyruus.com>
2015-10-25 21:26:02 -0400

    Added gulp tasks to move material-design-lite files into extension dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var browserify = require('browserify')
 var gulp = require('gulp');
+var uglify = require('gulp-uglify')
 var source = require('vinyl-source-stream')
+
+// TODO: Rename "extension" directory to "dist"
 
 gulp.task('browserify', function() {
     return browserify({
@@ -11,4 +14,18 @@ gulp.task('browserify', function() {
             .pipe(source('youtube-queue.js'))
             .pipe(gulp.dest('./extension/js/'))
 })
-gulp.task('default', ['browserify'])
+
+gulp.task('material-js', function() {
+    return gulp.src('./node_modules/material-design-lite/material.js')
+            .pipe(uglify())
+            .pipe(gulp.dest('./extension/js/'))
+})
+
+gulp.task('material-css', function() {
+    return gulp.src('./node_modules/material-design-lite/material.css')
+            .pipe(gulp.dest('./extension/css/'))
+})
+
+
+gulp.task('material', ['material-js', 'material-css'])
+gulp.task('default', ['browserify', 'material'])
