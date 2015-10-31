c79d43b54aa5ed42b0d28cce733c7bfaf303e0f8
Paavan Bhavsar <pbhavsar@kyruus.com>
2015-10-31 18:35:37 -0400

    Created basic template for widget

--- src/app.js
+++ src/app.js
@@ -4,3 +4,9 @@ widget = new QueueWidget({
     "el": "body",
     "append": true
 })
+
+// Inject google fonts for icons
+var link = document.createElement("link");
+link.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
+link.rel = "stylesheet";
+document.getElementsByTagName("head")[0].appendChild(link);
